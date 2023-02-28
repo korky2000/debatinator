6ab4a957b011436052fc10d767a24f7dffdee7d6
korky2000 <54334318+korky2000@users.noreply.github.com>
2023-02-28 14:03:55 -0500

    Update resolve.js
    added strikethru

--- resolve.js
+++ resolve.js
@@ -1,4 +1,73 @@
-﻿function capFirst(string) {
+//make strikethrough function
+//jquery???
+//this is my punishment for being gay
+//yay i did it! 
+
+function strikeThru() {
+    const textElement = document.getElementById('random_Resolve');
+    const textElement2 = document.getElementById('random_Resolve2');
+    const textElement3 = document.getElementById('random_Resolve3');
+    const textElement4 = document.getElementById('random_Resolve4');
+    const textElement5 = document.getElementById('random_Resolve5');
+
+
+    textElement.addEventListener('click', function () {
+
+        if (textElement.style.color === 'red' ) {
+
+            textElement.style.color = 'black';
+        }
+        else {
+            textElement.style.color = 'red';
+        }
+    });
+    textElement2.addEventListener('click', function () {
+
+        if (textElement2.style.color === 'red') {
+
+            textElement2.style.color = 'black';
+        }
+        else {
+            textElement2.style.color = 'red';
+        }
+    });
+    textElement3.addEventListener('click', function () {
+
+        if (textElement3.style.color === 'red') {
+
+            textElement3.style.color = 'black';
+        }
+        else {
+            textElement3.style.color = 'red';
+        }
+    });
+
+    textElement4.addEventListener('click', function () {
+
+        if (textElement4.style.color === 'red') {
+
+            textElement4.style.color = 'black';
+        }
+        else {
+            textElement4.style.color = 'red';
+        }
+    });
+    textElement5.addEventListener('click', function () {
+
+        if (textElement5.style.color === 'red') {
+
+            textElement5.style.color = 'black';
+        }
+        else {
+            textElement5.style.color = 'red';
+        }
+    });
+
+
+}
+
+
+function capFirst(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
@@ -21,6 +90,33 @@ function generatePolicy() {
 
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
     document.getElementById("random_Resolve").innerHTML = name;
+
+
+}
+function generatePolicy5() {
+    var prefix = [
+        "TH should", "TH ought to", "The USFG should", "The USFG ought to"
+
+    ];
+    var body = [
+        "increase minimum wage to $20 an hour", "abolish student loans", "regulate big tech's influence on privacy", "decrease minimum wage",
+        "impose a wage cap for professional sports", "legalize all drugs", "fund the police", "defund the police", "give reperations to African Americans",
+        "increase the scope of the patriot act", "increase conservation efforts in areas affected by chemical disasters", "institute UBI",
+        "codify Roe v Way into law", "increase the frequency of gun buyback programs", "ban Red Dye 40 in foods", "increase funding for arts programs",
+        "university education should be free", "should fund research into ketamine treatments for depression", "legalize recreational marijuana",
+        "abolish for profit prisons", "ban cosmetic surgery", "permit the use of performance enhancing drugs in professional sports"
+    ];
+
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve2").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve3").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve4").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve5").innerHTML = name;
 }
 function generateValues() {
     var prefix = [
@@ -47,7 +143,43 @@ function generateValues() {
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " "+ (qualifier[getRandomInt(0, qualifier.length)]) + " "+ (value2[getRandomInt(0, value2.length)]);
     document.getElementById("random_Resolve").innerHTML = name;
 }
+
+function generateValues5() {
+    var prefix = [
+        "TH believes that", "TH thinks",
+
+    ];
+    var value1 = [
+        "big business", "privacy", "profits", "enviromental conservation", "military strength", "big tech", "FDA oversite", "human rights",
+        "workers rights", "unionization efforts", "abortion rights", "security", "eliminating student debt", "free college", "social security",
+        "for profit prisons", "opioid oversodes"
+
+    ];
+
+    var qualifier = [
+        "is/are more important than", "is/are less important than", "trumps"
+
+    ];
+
+    var value2 = [
+        "big business", "privacy", "profits", "enviromental conservation", "military strength", "big tech", "FDA oversite", "human rights",
+        "workers rights", "unionization efforts", "security", "expansion", "corperate expansion", "the death penalty"
+    ];
+
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
+    document.getElementById("random_Resolve").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
+    document.getElementById("random_Resolve2").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
+    document.getElementById("random_Resolve3").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
+    document.getElementById("random_Resolve4").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
+    document.getElementById("random_Resolve5").innerHTML = name;
+}
+
 function generateFact() {
+
     var prefix = [
         "TH believes that", "TH thinks",
 
@@ -72,31 +204,97 @@ function generateFact() {
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
     document.getElementById("random_Resolve").innerHTML = name;
 }
+
+function generateFact5() {
+    var prefix = [
+        "TH believes that", "TH thinks",
+
+    ];
+    var value1 = [
+        "big business", "privacy", "profits", "enviromental conservation", "military strength", "big tech", "FDA oversite", "human rights",
+        "workers rights", "unionization efforts", "free university education", "eliminating student debt", "legalizing drugs", "medical marijuana legalization",
+        "ketamine treatments for treatment resistant depression", "legalization of recreational marijuana use", "opioid harm reduction",
+        "opioid addiction"
+
+    ];
+
+
+    var value2 = [
+        "benificial", "unimportant", "necessary", "unnecessary", "important"
+    ];
+    var importance = [
+        "for economic development", "for the good of society", "for human rights", "for national security", "for economic policy",
+        "for international policy", "for social development", "for the betterment of society", "for the advancement of the American people"
+    ];
+   
+
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve2").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve3").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve4").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve5").innerHTML = name;
+}
+
 function genPolicy() {
+    var amountSelect = document.getElementById('amount');
+    var amountValue = amountSelect.options[amountSelect.selectedIndex].value;
+    console.log(amountValue);
     var resolutionSelect = document.getElementById('resolve');
     var resolutionValue = resolutionSelect.options[resolutionSelect.selectedIndex].value;
     console.log(resolutionValue);
-    if (resolutionValue == 'policy') {
+    if (resolutionValue == 'policy' && amountValue == 'one') {
         generatePolicy();
+    }
+    if (resolutionValue == 'policy' && amountValue == 'five') {
+        generatePolicy5();
+    }
+    if (resolutionValue == 'fact' && amountValue == 'ten') {
+        generateFact();
     }
 }
 function genFact() {
     var resolutionSelect = document.getElementById('resolve');
     var resolutionValue = resolutionSelect.options[resolutionSelect.selectedIndex].value;
     console.log(resolutionValue);
-    if (resolutionValue == 'fact') {
+    var amountSelect = document.getElementById('amount');
+    var amountValue = amountSelect.options[amountSelect.selectedIndex].value;
+    console.log(amountValue);
+
+    if (resolutionValue == 'fact' && amountValue == 'one') {
+        generateFact();
+    }
+    if (resolutionValue == 'fact' && amountValue == 'five') {
+        generateFact5();
+    }
+    if (resolutionValue == 'fact' && amountValue == 'ten') {
         generateFact();
     }
 }
-        function genValue() {
+function genValue() {
+    var amountSelect = document.getElementById('amount');
+    var amountValue = amountSelect.options[amountSelect.selectedIndex].value;
+    console.log(amountValue);
             var resolutionSelect = document.getElementById('resolve');
             var resolutionValue = resolutionSelect.options[resolutionSelect.selectedIndex].value;
             console.log(resolutionValue);
-            if (resolutionValue == 'value') {
-                generateValues();
+            if (resolutionValue == 'value' && amountValue == 'one') {
+                generateValue();
+            }
+            if (resolutionValue == 'value' && amountValue == 'five') {
+                generateValues5();
+            }
+            if (resolutionValue == 'fact' && amountValue == 'ten') {
+                generateFact();
             }
         }
-    function generateRoom() {
+function generateRoom() {
+    var element = document.getElementById("clickMe");
+    element.className = "clickMe";
         genPolicy();
         genFact();
         genValue();
