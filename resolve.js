a6798741ed4eead9757e5d9bf346b8654a12c86a
korky2000 <54334318+korky2000@users.noreply.github.com>
2023-02-28 15:34:55 -0500

    Update resolve.js
    added mixed and to do mix

--- resolve.js
+++ resolve.js
@@ -1,7 +1,8 @@
-//make strikethrough function
-//jquery???
-//this is my punishment for being gay
-//yay i did it! 
+//to do
+//-MORE RESOLUTIONS
+//integrate arrays with mySQL database (ew php)
+//Some sort of news API that integrates current events
+//IPDA tab
 
 function strikeThru() {
     const textElement = document.getElementById('random_Resolve');
@@ -118,6 +119,26 @@ function generatePolicy5() {
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
     document.getElementById("random_Resolve5").innerHTML = name;
 }
+function generatePolicyMix() {
+    var prefix = [
+        "TH should", "TH ought to", "The USFG should", "The USFG ought to"
+
+    ];
+    var body = [
+        "increase minimum wage to $20 an hour", "abolish student loans", "regulate big tech's influence on privacy", "decrease minimum wage",
+        "impose a wage cap for professional sports", "legalize all drugs", "fund the police", "defund the police", "give reperations to African Americans",
+        "increase the scope of the patriot act", "increase conservation efforts in areas affected by chemical disasters", "institute UBI",
+        "codify Roe v Way into law", "increase the frequency of gun buyback programs", "ban Red Dye 40 in foods", "increase funding for arts programs",
+        "university education should be free", "should fund research into ketamine treatments for depression", "legalize recreational marijuana",
+        "abolish for profit prisons", "ban cosmetic surgery", "permit the use of performance enhancing drugs in professional sports"
+    ];
+
+  
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve4").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (body[getRandomInt(0, body.length)]);
+    document.getElementById("random_Resolve5").innerHTML = name;
+}
 function generateValues() {
     var prefix = [
         "TH believes that", "TH thinks", 
@@ -143,7 +164,6 @@ function generateValues() {
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " "+ (qualifier[getRandomInt(0, qualifier.length)]) + " "+ (value2[getRandomInt(0, value2.length)]);
     document.getElementById("random_Resolve").innerHTML = name;
 }
-
 function generateValues5() {
     var prefix = [
         "TH believes that", "TH thinks",
@@ -177,7 +197,33 @@ function generateValues5() {
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
     document.getElementById("random_Resolve5").innerHTML = name;
 }
+function generateValuesMix() {
+    var prefix = [
+        "TH believes that", "TH thinks",
 
+    ];
+    var value1 = [
+        "big business", "privacy", "profits", "enviromental conservation", "military strength", "big tech", "FDA oversite", "human rights",
+        "workers rights", "unionization efforts", "abortion rights", "security", "eliminating student debt", "free college", "social security",
+        "for profit prisons", "opioid oversodes"
+
+    ];
+
+    var qualifier = [
+        "is/are more important than", "is/are less important than", "trumps"
+
+    ];
+
+    var value2 = [
+        "big business", "privacy", "profits", "enviromental conservation", "military strength", "big tech", "FDA oversite", "human rights",
+        "workers rights", "unionization efforts", "security", "expansion", "corperate expansion", "the death penalty"
+    ];
+
+    
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " " + (qualifier[getRandomInt(0, qualifier.length)]) + " " + (value2[getRandomInt(0, value2.length)]);
+    document.getElementById("random_Resolve3").innerHTML = name;
+   
+}
 function generateFact() {
 
     var prefix = [
@@ -239,6 +285,39 @@ function generateFact5() {
     var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
     document.getElementById("random_Resolve5").innerHTML = name;
 }
+function generateFactMix() {
+    var prefix = [
+        "TH believes that", "TH thinks",
+
+    ];
+    var value1 = [
+        "big business", "privacy", "profits", "enviromental conservation", "military strength", "big tech", "FDA oversite", "human rights",
+        "workers rights", "unionization efforts", "free university education", "eliminating student debt", "legalizing drugs", "medical marijuana legalization",
+        "ketamine treatments for treatment resistant depression", "legalization of recreational marijuana use", "opioid harm reduction",
+        "opioid addiction"
+
+    ];
+
+
+    var value2 = [
+        "benificial", "unimportant", "necessary", "unnecessary", "important"
+    ];
+    var importance = [
+        "for economic development", "for the good of society", "for human rights", "for national security", "for economic policy",
+        "for international policy", "for social development", "for the betterment of society", "for the advancement of the American people"
+    ];
+
+
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve").innerHTML = name;
+    var name = capFirst(prefix[getRandomInt(0, prefix.length)]) + " " + (value1[getRandomInt(0, value1.length)]) + " is " + " " + (value2[getRandomInt(0, value2.length)]) + " " + (importance[getRandomInt(0, importance.length)]);
+    document.getElementById("random_Resolve2").innerHTML = name;
+}
+function generateMixed() {
+    generateFactMix();
+    generateValuesMix();
+    generatePolicyMix();
+}
 
 function genPolicy() {
     var amountSelect = document.getElementById('amount');
@@ -291,12 +370,30 @@ function genValue() {
             if (resolutionValue == 'fact' && amountValue == 'ten') {
                 generateFact();
             }
-        }
+}
+function genMixed() {
+    var amountSelect = document.getElementById('amount');
+    var amountValue = amountSelect.options[amountSelect.selectedIndex].value;
+    console.log(amountValue);
+    var resolutionSelect = document.getElementById('resolve');
+    var resolutionValue = resolutionSelect.options[resolutionSelect.selectedIndex].value;
+    console.log(resolutionValue);
+    if (resolutionValue == 'mixed' && amountValue == 'one') {
+        generateMixed();
+    }
+    if (resolutionValue == 'mixed' && amountValue == 'five') {
+        generateMixed();
+    }
+    if (resolutionValue == 'mixed' && amountValue == 'ten') {
+        generateMixed();
+    }
+}
 function generateRoom() {
     var element = document.getElementById("clickMe");
     element.className = "clickMe";
         genPolicy();
         genFact();
-        genValue();
+    genValue();
+    genMixed();
+}
 
-    }
